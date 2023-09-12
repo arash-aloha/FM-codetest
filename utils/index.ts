adcc310a4c30474f7fe52fc1637bc46efc116d1a
arash-aloha <arash.a@live.se>
2023-09-12 09:49:48 +0200

    deleteing un-used code

--- utils/index.ts
+++ utils/index.ts
@@ -30,49 +30,10 @@ const getTime = function convertTimeToString(timeString: string) {
   return `${formattedHours}:${formattedMinutes}`;
 };
 
-const getWeekDay = (data) => {
-  {
-    data.forEach((item) => {
-      const timestampDate = new Date(item.duration.start);
-      console.log("timestampdate", timestampDate);
-      const weekday = timestampDate.getDay();
-      console.log("wwekday", weekday);
-      const groupedData = {};
-      if (!groupedData[weekday]) {
-        groupedData[weekday] = [];
-      }
-      groupedData[weekday].push(item);
-
-      const weekdays = [
-        "Sunday",
-        "Monday",
-        "Tuesday",
-        "Wednesday",
-        "Thursday",
-        "Friday",
-        "Saturday",
-      ];
-
-      for (let weekday in groupedData) {
-        if (groupedData.hasOwnProperty(weekday)) {
-          const dayOfWeek = weekdays[weekday];
-          const objects = groupedData[weekday];
-
-          console.log(`Cards for ${dayOfWeek}:`);
-          objects.forEach((obj) => {
-            console.log(`- ${obj.name}`);
-          });
-        }
-      }
-    });
-  }
-};
-
 export {
   createEndpointUrl,
   getCurrentTimestamp,
   getTimestamp6DaysAhead,
   getTime,
   getEncodedTimeStamp,
-  getWeekDay,
 };
